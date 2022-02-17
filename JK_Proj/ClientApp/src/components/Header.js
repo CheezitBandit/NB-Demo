e0c2a197314a502b4508b45cfbd4ff40e5f76f5f
Jessica Geiger <V359R962@wichita.edu>
2022-02-16 22:11:24 -0700

    Adding images to header, resizing, columns at base

---
+++ JK_Proj/ClientApp/src/components/Header.js
@@ -0,0 +1,22 @@
+﻿import React from 'react';
+import './Header.css';
+import heroUrl from '../img/hero.jpg';
+import canUrl from '../img/cans.png';
+
+
+export class Header extends React.Component {
+    static displayName = Header.name;
+
+    constructor(props) {
+        super(props);
+        }
+
+    render() {
+        return (
+            <div>
+            <img class="Header" src={heroUrl} alt="Header" />
+            <img class="Center" src={canUrl} alt="Cans" />
+</div>
+                );
+    }
+}
--- JK_Proj/ClientApp/src/components/Layout.js
+++ JK_Proj/ClientApp/src/components/Layout.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { Container } from 'reactstrap';
-import { NavMenu } from './NavMenu';
+import { Header } from './Header';
 
 export class Layout extends Component {
   static displayName = Layout.name;
 
   render () {
     return (
-      <div>
-        <NavMenu />
+        <div>
+        <Header />
         <Container>
           {this.props.children}
         </Container>
